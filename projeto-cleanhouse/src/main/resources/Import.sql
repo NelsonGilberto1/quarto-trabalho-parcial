886a96a0e6de687be49a37b493ef4294ee750f47
NelsonGilberto1 <168493132+NelsonGilberto1@users.noreply.github.com>
2025-10-13 17:41:42 +0000

    populando-tabelas

---
+++ projeto-cleanhouse/src/main/resources/Import.sql
@@ -0,0 +1,22 @@
+--Clientes--
+
+INSERT INTO Cliente (id,cpf,email,endereco,nome,telefone) VALUES ('01', '123.456.789-10', 'Antonio@email.com', 'Rua Tatu Bola numero 08', 'Antonio da Silva', '14 98203787');
+INSERT INTO Cliente (cpf,email,endereco,nome,telefone) VALUES ('23.456.7891-10', 'Bob@email.com', 'Rua Tamandua bandeira numero 02', 'Bob Luiz', '14 20938023');
+INSERT INTO Cliente (cpf,email,endereco,nome,telefone) VALUES ('3.456.78912-10', 'Carlinhos@email.com', 'Rua Macaco Prego 06', 'Carlinhos Cavalo', '14 293823329');
+INSERT INTO Cliente (cpf,email,endereco,nome,telefone) VALUES ('456.789.123-10', 'Daniel@email.com', 'Rua Onça pintada 07', 'Daniel Ficada', '14 392820938');
+INSERT INTO Cliente (cpf,email,endereco,nome,telefone) VALUES ('567.891.234-10', 'Enock@email.com', 'Rua Mico leão dorado 28', 'Enock Pereira', '14 29832802');
+
+--Empregada--
+
+INSERT INTO Empregada (dataCadastro, dataNascimento, id, status, descricao, email, endereco, nome, telefone) VALUES ('2025-09-01 10:00:00', '1999-02-05','001', true, 'Empregada domestica especialisada em trabalhos pesados', 'AnaLuiza@gmail.com', 'Rua Anaconda numero 99', 'Ana Luiza', '273926273');
+INSERT INTO Empregada (dataCadastro, dataNascimento,status, descricao, email, endereco, nome, telefone) VALUES ('2025-08-21 10:00:00', '1988-12-25',false, 'Empregada especialisada em polimento', 'BiancaGames@gmail.com', 'Rua Besouro numero 02', 'Bianca Almeida', '328322038');
+INSERT INTO Empregada (dataCadastro, dataNascimento,status, descricao, email, endereco, nome, telefone) VALUES ('2025-03-22 10:00:00', '1998-08-29',true, 'jardineira proficional', 'CleideSilva@gmail.com', 'Rua Canguru numero 102', 'Cleide da Silva', '202303289');
+INSERT INTO Empregada (dataCadastro, dataNascimento,status, descricao, email, endereco, nome, telefone) VALUES ('2025-05-30 10:00:00', '1997-02-19',false, 'Cozinheira de restaurante', 'DaniFicada@gmail.com', 'Rua Doninha numero 72', 'Dani Ficada', '232092382');
+INSERT INTO Empregada (dataCadastro, dataNascimento,status, descricao, email, endereco, nome, telefone) VALUES ('2025-07-01 10:00:00', '1999-01-28',false, 'Empregada domestica especialisada em trabalhos pesados', 'EloisaMosk@gmail.com', 'Rua Elefante numero 23', 'Eloisa Souza', '9238i2038');
+
+
+--Avaliacao--
+
+
+
+
